c3a0accb0691e6425116a7e0eb80c35a3d7b5bb5
Caio de Souza <caiodesouza.cds@gmail.com>
2026-03-16 20:14:52 -0300

    Criado conexao com banco

---
+++ src/main/java/org/task_manager/dao/BaseDAO.java
@@ -0,0 +1,14 @@
+package org.task_manager.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class BaseDAO {
+
+    protected Connection conn() throws SQLException {
+
+        return ConnectionFactory.getInstance().get();
+
+    }
+
+}
---
+++ src/main/java/org/task_manager/dao/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package org.task_manager.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static ConnectionFactory instance;
+
+    public ConnectionFactory() {
+    }
+
+    public static ConnectionFactory getInstance() {
+        if(instance == null) instance = new ConnectionFactory();
+        return instance;
+    }
+
+    public Connection get() throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/teste";
+        String user = "root";
+        String password = "univille";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+        return conn;
+    }
+
+}
